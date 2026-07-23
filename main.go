35732d84c6a3f6088133cebc8cbab74778410ee8
waiting-here <dify2api@sweet-home.io>
2026-07-23 17:04:06 -0400

    chore: bump version to 1.0.0-beta.1
    - main.go: Version constant updated from 1.0.0-alpha.4 to 1.0.0-beta.1

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 // Version is the Dify2API release version, printed by the -version flag.
 // 发版时与 git tag 同步（tag 格式 v<major.minor.patch>）。
-const Version = "1.0.0-alpha.4"
+const Version = "1.0.0-beta.1"
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
