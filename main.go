e3ad7a6d0ca533c4203c575610e49d5cb217a3e0
waiting-here <dify2api@sweet-home.io>
2026-08-05 02:46:14 -0400

    docs: changelog for v1.2.1; bump version to 1.2.1

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 
 // Version is the Dify2API release version, printed by the -version flag.
 // 发版时与 git tag 同步（tag 格式 v<major.minor.patch>）。
-const Version = "1.2.0"
+const Version = "1.2.1"
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
